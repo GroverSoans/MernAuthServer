11060156435f6bf5743759e2c572484cd72a9b39
Grover Soans <groverpsoans@gmail.com>
2024-12-13 09:49:13 -0600

    test 6

--- src/index.ts
+++ src/index.ts
@@ -20,19 +20,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: (origin, callback) => {
-        // Allow requests without an origin (like from Postman or other API clients)
-        if (!origin || allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-        // Reject requests from unknown origins
-        return callback(new Error('Not allowed by CORS'), false);
-    },
-    credentials: true,  // Allow cookies/credentials to be sent
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+    origin: '*',      // Allow only this frontend to access the backend
+    credentials: true,       // Allow cookies or credentials to be sent
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization']  // Allowed headers
 }));
-  
 // Handle preflight (OPTIONS) requests for all routes
 app.options('*', cors()); 
 
